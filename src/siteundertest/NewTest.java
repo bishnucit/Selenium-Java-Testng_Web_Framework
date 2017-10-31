a2556b86d1e246f369c49db13e03cce9adb22a98
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2017-10-31 19:58:56 +0530

    first commit

---
+++ src/siteundertest/NewTest.java
@@ -0,0 +1,33 @@
+package siteundertest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+	public WebDriver driver = new FirefoxDriver();
+	String appUrl = "https://www.google.com";
+	String expectedTitle = "Google";
+  @Test
+  public void googlesearch() {
+	  driver.get(appUrl);
+	  driver.manage().window().maximize();
+	  String actualTitle = driver.getTitle();
+	  Assert.assertEquals(expectedTitle,actualTitle);
+	  
+	  
+	  WebElement username = driver.findElement(By.id("lst-ib"));
+	  username.clear();
+	  username.sendKeys("Selenium");
+	  WebElement SearchButton = driver.findElement(By.xpath("//*[@id='tsf']/div[2]/div[3]/center/input[1]"));
+	  SearchButton.click();
+	  
+	  Assert.assertEquals(driver.getTitle(),"Selenium - Google Search");
+	  
+	  driver.quit();
+
+  }
+}
